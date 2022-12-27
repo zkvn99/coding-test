c0aaf348a7a01a7e7482ee65f85bff183ebf3b08
Minuk Lee <zkvn1103@naver.com>
2022-12-27 23:19:11 +0900

    1주차[1]
    완주하지 못한 선수 풀이

---
+++ lv-1/완주하지_못한_선수.js
@@ -0,0 +1,14 @@
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+    var answer = '';
+    for (let i=0; i<participant.length; i++)
+        {
+            if(participant[i] !== completion[i])
+            {
+                answer = participant[i];
+                break;
+            }
+        }
+    return answer;
+}
