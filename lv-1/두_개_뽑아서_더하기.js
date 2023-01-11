8bc821907a121454ce52834e94d37d2ab7488c02
이민욱 <56884540+zkvn99@users.noreply.github.com>
2023-01-11 22:39:02 +0900

    Update 두_개_뽑아서_더하기.js

--- lv-1/두_개_뽑아서_더하기.js
+++ lv-1/두_개_뽑아서_더하기.js
@@ -1,19 +1,21 @@
-function solution(lottos, win_nums) {
-    var first = 7;
-    var second = 1;
-    lottos.forEach(e => {if(win_nums.indexOf(e) > -1 || e == 0)
-        first -= 1;
-                         if(win_nums.indexOf(e) == -1)
-                             second += 1;});
-    if (second > 6)
-        second = 6;
-
-    if (first >= 6)
-        first = 6;
-
-    return answer = [first,second];
+function solution(numbers) {
+    var num = numbers;
+    var answer = [];
+    var s = 0;
+    for (let i=0; i<num.length; i++)
+        {
+            for (let j=1; j<num.length; j++)
+                {
+                    if(i<j)
+                    answer[s++] = num[i] + num[j]; 
+                }
+        }
+    let set = new Set(answer);
+    set = [...set];
+    answer = set.sort(compare);
+    return answer;
 }
-// 최고순위 최저순위 값 지정 후
-// indexOf 활용 값이 찾아지면 최고순위 값 수정
-// 반대의 경우 최저순위 값 수정
-// 6개 or 5개 틀렸을경우 등수가 같아 if로 값 동결
+
+function compare(a,b){
+    return a - b;
+}
