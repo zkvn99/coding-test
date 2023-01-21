5e661b90f7a50e2c6c83e77722edcf3fd2cfa7b2
이민욱 <56884540+zkvn99@users.noreply.github.com>
2023-01-21 16:05:15 +0900

    [4주차][2]
    최소직사각형

--- lv-1/최소직사각형.js
+++ lv-1/최소직사각형.js
@@ -4,7 +4,7 @@ function solution(sizes) {
     let size = (a,b) => a*b; // 넓이
     sizes.map((size,i) => { // size 배열 값, i 인덱스
         width[i] = Math.max(...size); // 큰 값을 가로에 넣고
-        height[i] = Math.min(...size); // 작은 값을 세로에 넣음
-    })
+        height[i] = Math.min(...size); // 작은 값을 세로에 넣음 
+    }) // 가로와 세로가 바뀌어도 상관없으므로 큰 값, 작은 값으로 나눔
     return size(Math.max(...width),Math.max(...height)); // 각자 넣은 값에서 가장 큰 값을 추출 후 넓이
 }
